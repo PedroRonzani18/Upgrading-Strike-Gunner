14eb51763aa493abc01343ce498a2e21e515c468
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2022-12-25 19:07:36 -0300

    leitura direto pelo source + kamikaze implementados

--- GeneralFiles/Scopes/Wave.cpp
+++ GeneralFiles/Scopes/Wave.cpp
@@ -11,27 +11,23 @@ std::vector<Enemy> waveTemplateGeneral(const char* path)
     std::vector<Enemy> aux;
     Parser aP = Parser::parseGeneral(path);
 
-
     std::vector<int> rands;
 
     for(int i=0; i<aP.linhas; i++)
     {       
-        if(!aP.randMinus.empty())
-            for(int j = 0; j < aP.numberOfEnemies[i]; j++)
-                rands.push_back(rand()%aP.randResto[i] - aP.randMinus[i]);
+        if(!aP.randMinus.empty())   for(int j = 0; j < aP.numberOfEnemies[i]; j++)
+                                        rands.push_back(rand()%aP.randResto[i] - aP.randMinus[i]);
             
         for(int j = 0; j < aP.numberOfEnemies[i]; j++)
         {
             Enemy e1 = Enemy(aP.type[i],aP.typeMove[i],aP.numberOfShots[i],aP.typeTiroManager[i],aP.vx[i],aP.vy[i]);
 
-            if(!aP.randMinus.empty()){
-            e1.setMidPoint(aP.midConstX[i] * rands[j] + j * aP.multiplyerX[i], 
-                           aP.midConstY[i]            + j * aP.multiplyerY[i]);
-            }
-            else{
-               e1.setMidPoint(aP.midConstX[i] + j * aP.multiplyerX[i],
-                              aP.midConstY[i] + j * aP.multiplyerY[i]); 
-            }
+            if(!aP.randMinus.empty())
+                e1.setMidPoint(aP.midConstX[i] * rands[j] + j * aP.multiplyerX[i], aP.midConstY[i] + j * aP.multiplyerY[i]);
+            
+            else
+               e1.setMidPoint(aP.midConstX[i]             + j * aP.multiplyerX[i], aP.midConstY[i] + j * aP.multiplyerY[i]); 
+            
             if(!aP.textureId.empty()) e1.setDisplayListModel(textures[aP.textureId[i]]);
             if(!aP.resize.empty())    e1.setResize(aP.resize[i]);
             if(!aP.maxX.empty())      e1.setMax(aP.maxX[i],aP.maxY[i]);
@@ -42,19 +38,4 @@ std::vector<Enemy> waveTemplateGeneral(const char* path)
         }
     }
     return aux;
-}
-
-std::vector<Enemy> waveKamikaseAcelerado18()
-{
-    std::vector<Enemy> aux;
-
-    Enemy e1 = Enemy(0, 12, 0, 0, -2, -2);
-
-    int r;
-    for(int i = 0; i < 40; i++){
-        r = rand()%17 - 8;
-        e1.setMidPoint(10*r, 150 + i*40);
-        aux.push_back(e1);
-    }
-    return aux;  
 }
